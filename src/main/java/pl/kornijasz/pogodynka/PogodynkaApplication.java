fc6a9e463720b7bc1417969857fc16b64ac864e9
pkornijasz <pk@nkk.pl>
2021-05-03 00:58:31 +0200

    Initial commit

---
+++ src/main/java/pl/kornijasz/pogodynka/PogodynkaApplication.java
@@ -0,0 +1,13 @@
+package pl.kornijasz.pogodynka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PogodynkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PogodynkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/kornijasz/pogodynka/PogodynkaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kornijasz.pogodynka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PogodynkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
