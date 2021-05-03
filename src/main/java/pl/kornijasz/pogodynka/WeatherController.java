0ab3b6f9b143c8ae97ba181b92dd6ced68c6d021
pkornijasz <pk@nkk.pl>
2021-05-03 15:29:56 +0200

    Final+Icon+CSS!

--- src/main/java/pl/kornijasz/pogodynka/WeatherController.java
+++ src/main/java/pl/kornijasz/pogodynka/WeatherController.java
@@ -47,8 +47,6 @@ public class WeatherController {
             return weather;
         } catch (HttpStatusCodeException e) {
             return null;
-        } catch (UnknownContentTypeException e) {
-            return null;
         }
     }
 
